6fcad148daadd9a907065a595c5a9a8ecb50c540
Sai-RWC <saimon_culanag@dlsu.edu.ph>
2025-10-24 15:36:43 +0800

    Added https certificates for localhost testing

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import pages  # importing registers all routes
 
 # Windows-safe guard (covers multiprocessing import name)
 if __name__ in {"__main__", "__mp_main__"}:
-    ui.run(title='LeTARA', reload=False)
+    ui.run(title='LeTARA', reload=False, ssl_certfile='localhost.crt', ssl_keyfile='localhost.key')
